ce6bf466ff6b352ff91868baf2b9eef2510aa69e
YunHo Choi <62130211+YouKnowGit@users.noreply.github.com>
2025-05-26 17:44:17 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    if (0 < angle && angle < 90)         return 1;       // 예각
+    else if (angle == 90)       return 2;       // 직각
+    else if (90 < angle && angle < 180)  return 3;       // 둔각
+    else if (angle == 180)      return 4;       // 평각
+}
