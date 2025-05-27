82a0ef4f915e1e5b03075359c12ceea378dab13e
YunHo Choi <62130211+YouKnowGit@users.noreply.github.com>
2025-05-27 11:23:06 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -4,9 +4,6 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
-    for (int i = 2; i <= n; i += 2) {
-        answer += i;
-    }
-    return answer;
+    int k = n / 2;  // 짝수 개수
+    return k * (k + 1);
 }
