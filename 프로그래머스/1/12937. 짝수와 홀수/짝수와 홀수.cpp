e916ac84c0fb8dfa83b49484c6cffe717ae52544
YunHo Choi <62130211+YouKnowGit@users.noreply.github.com>
2025-05-29 09:05:26 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -5,13 +5,6 @@ using namespace std;
 
 string solution(int num) {
     string answer = "";
-    if (num % 2  == 0)
-    {
-        answer = "Even";
-    }
-    else
-    {
-        answer = "Odd";
-    }
-    return answer;
+
+    return num & 1 ? "Odd" : "Even";
 }
