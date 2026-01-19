4f34119949a504debc40e49f642f3fd5c3f6273f
YunHo Choi <62130211+YouKnowGit@users.noreply.github.com>
2026-01-19 18:17:54 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0;
+    
+    for (int h : array)
+    {
+        if (h > height)
+        {
+            answer ++;
+        }
+    }
+    
+    return answer;
+}
